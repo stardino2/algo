7a06e93d08a3a3377750835dddd4e941716cb36e
stardino2 <83866706+stardino2@users.noreply.github.com>
2021-06-04 13:27:50 +0900

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+struct DATA {
+  int a1, a2, a3;
+  DATA() {}
+  DATA(int a1, int a2, int a3) : a1(a1), a2(a2), a3(a3){}
+  bool operator == (const DATA&A) const {
+    return a1 == A.a1 && a2 == A.a2 && a3 == A.a3;
+  }
+  bool operator !=(const DATA &A) const {
+    return a1 != A.a1 || a2 != A.a2 || a3 != A.a3;
+  }
+};
