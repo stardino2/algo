8a5fb66dc0c26c7ab7abc4ab97d1afdfd1e78e67
stardino2 <83866706+stardino2@users.noreply.github.com>
2021-06-04 13:45:30 +0900

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -11,3 +11,25 @@ struct DATA {
     return a1 != A.a1 || a2 != A.a2 || a3 != A.a3;
   }
 };
+
+class hash_table{
+  public:
+  const static int SIZE = 10000;
+  struct NODE{
+    DATA value;
+    NODE* next;
+    NODE() {}
+    NODE(DATA value, NODE* next): value(value), next(next){}
+  };
+  NODE *table[SIZE];
+  hash_table(){
+    for(int i = 0; i < SIZE; i++) table[i] = NULL;
+  }
+  int hash(DATA &A){
+    return (A.a1 + A.a2 + A.a3) % SIZE;
+  }
+  void insert(DATA key) {
+    int index = hash(key);
+    table[index] = new NODE(key, table[index]);
+  }
+}
